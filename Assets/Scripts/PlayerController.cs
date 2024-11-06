592f4daf56d7a42f5bcc09b685b2ab4ef59bd5ba
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-06 16:26:51 +0100

    an attempt to add wallsliding unsuccesful due to the player not moving down when colliding with a Wall.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,61 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    [SerializeField]
-    float jumpForce = 10;
-    [SerializeField]
-    float speed = 10;
-    [SerializeField]
-    Transform groundCheccker;
-    [SerializeField]
-    LayerMask groundLayer;
-
+{   
+    private float xMove;
+    [SerializeField] float jumpForce = 10;
+    [SerializeField] float speed = 8f; 
     bool mayJump = true;
+    private bool isFacingRight = false;
+
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;     
+    private bool canDash = true;
+    private bool isDashing;    
+    
+    private bool iswallSliding;
+    private float wallSlidingSpeed = 2f;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private Transform wallChecker;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
 
     void Update()
     {
+
+        if (isDashing)
+        {
+            return;
+        }
+
         float xMove = Input.GetAxisRaw("Horizontal");
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new(
-            xMove * speed,
-            rb.velocity.y
-        );
+        rb.velocity = new ( xMove * speed, rb.velocity.y ); 
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        
+        WallSlide();
 
 
-        if (Input.GetAxisRaw("Jump") > 0 && mayJump == true)
+        if(Input.GetKeyDown(KeyCode.Q) && canDash == true)
         {
-            rb.AddForce(Vector2.up * jumpForce);
-            
-            mayJump = false;
-        }
-        if (rb.velocity.x > 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (rb.velocity.x < 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
+            StartCoroutine(Dash());
         }
 
-        if (Input.GetAxisRaw("Jump") == 0 && mayJump == false)
-        {
-            mayJump = true;
-        }
+        Flip();
+        
+    }
 
-        if(isGrounded() == false)
-        {
-            mayJump = false;
-        }
+    private void FixedUpdate() 
+    {
+        
     }
 
     private bool isGrounded()
     {
-        if (Physics2D.OverlapCircle(groundCheccker.position, .2f, groundLayer))
+        if (Physics2D.OverlapCircle(groundChecker.position, .2f, groundLayer))
         {
             return true;
         }
@@ -64,4 +83,68 @@ public class PlayerController : MonoBehaviour
             return false;
         }
     }
+
+    private bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wallChecker.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        // if (isWalled() && !isGrounded() && xMove != 0f)
+        // {
+        //     iswallSliding = true;
+        //     rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        // }
+        // else
+        // {
+        //     iswallSliding = false;
+        // }
+    }
+
+
+    private void Flip()
+    {
+            if (rb.velocity.x > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            if (rb.velocity.x < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+    }
+
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalgGavity = rb.gravityScale;
+        rb.gravityScale = 0;
+        if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * -dashingPower, 0f);
+        }
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalgGavity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    } 
+
+    // private void Flip()
+    // {
+    //     if (isFacingRight && xMove < 0f || !isFacingRight && xMove > 0f)
+    //     {
+    //         isFacingRight = !isFacingRight;
+    //         Vector3 localScale = transform.localScale;
+    //         localScale.x *= -1f;
+    //         transform.localScale = localScale;
+    //     }
+    // }
 }
