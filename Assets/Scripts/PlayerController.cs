39fe7b14131e2c008a10be9e99670b0786a8511a
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-12 09:59:52 +0100

    the great reset, micke hjälp nästa lektion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection.Emit;
-using System.Xml.Serialization;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -12,23 +11,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpForce = 10;
     [SerializeField] float speed = 8f; 
     bool mayJump = true;
-    private bool isFacingRight = false;
 
-    [SerializeField]
-    private float dashingPower = 28f;
+    private float dashingPower = 24f;
     private float dashingTime = 0.2f;
     private float dashingCooldown = 1f;     
     private bool canDash = true;
     private bool isDashing;    
-    
-    private bool iswallSliding;
-    private float wallSlidingSpeed = 2f;
-    private bool isWallJumping;
-    private float wallJumpingDirection;
-    private float wallJumpingTime = 0.2f;
-    private float wallJumpingCounter;
-    private float wallJumpingDuration = 0.4f;
-    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
 
     [SerializeField] private Rigidbody2D rb;
@@ -45,45 +33,44 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-        xMove = Input.GetAxisRaw("Horizontal");
-        
+        float xMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new ( xMove * speed, rb.velocity.y ); 
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-        
-        WallSlide();
-        WallJump();
+    
 
-        if(!isWallJumping)
-        {
-            Flip();
-        }
 
         if(Input.GetKeyDown(KeyCode.Q) && canDash == true)
         {
             StartCoroutine(Dash());
         }
+
+        Flip();
         
     }
-    
+
     private void FixedUpdate() 
     {
-        if(!isWallJumping)
-        {
-            rb.velocity = new Vector2(xMove * speed, rb.velocity.y); 
-        }
+        
     }
 
     private bool isGrounded()
     {
-        return Physics2D.OverlapCircle(groundChecker.position, 0.2f, groundLayer);
+        if (Physics2D.OverlapCircle(groundChecker.position, .2f, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     private bool isWalled()
@@ -91,68 +78,9 @@ public class PlayerController : MonoBehaviour
         return Physics2D.OverlapCircle(wallChecker.position, 0.2f, wallLayer);
     }
 
-    private void WallSlide()
-    {
-        if (isWalled() && !isGrounded() && xMove != 0f)
-        {
-            iswallSliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
-        }
-        else
-        {
-            iswallSliding = false;
-        } 
-    }
-
-        private void WallJump()
-    {
-        if (iswallSliding)
-        {
-            isWallJumping = false;
-            wallJumpingDirection = -transform.localScale.x;
-            wallJumpingCounter = wallJumpingTime;
-
-            CancelInvoke(nameof(StopWallJumping));
-        }
-        else
-        {
-            wallJumpingCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump") && wallJumpingCounter < 0f)
-        {
-            isWallJumping = true;
-            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
-            wallJumpingCounter = 0f;        
-            if(transform.localScale.x != wallJumpingDirection)
-            {
-                isFacingRight = !isFacingRight;
-                Vector3 localScale = transform.localScale;
-                localScale.x *= -1f;
-                transform.localScale = localScale;
-
-            }
-
-            Invoke(nameof(StopWallJumping), wallJumpingDirection);
-        }
-    }
-
-    private void StopWallJumping()
-    {
-        isWallJumping = false;
-    }
 
 
-    private void Flip()
-    {
-        if (!isFacingRight && xMove > 0 || isFacingRight && xMove < 0)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
+
 
 
     private IEnumerator Dash()
@@ -160,12 +88,44 @@ public class PlayerController : MonoBehaviour
         canDash = false;
         isDashing = true;
         float originalgGavity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        rb.gravityScale = 0;
+        if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * -dashingPower, 0f);
+        }
         yield return new WaitForSeconds(dashingTime);
         rb.gravityScale = originalgGavity;
         isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     } 
+
+    private void Flip()
+    {
+            if (rb.velocity.x > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            if (rb.velocity.x < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+    }
+    
+    // private void Flip()
+    // {
+    //     if (!isFacingRight && xMove < 0f || isFacingRight && xMove > 0f)
+    //     {
+    //         isFacingRight = !isFacingRight;
+    //         Vector3 localScale = transform.localScale;
+    //         localScale.x *= -1f;
+    //         transform.localScale = localScale;
+    //     }
+    // }
+
+
 }
